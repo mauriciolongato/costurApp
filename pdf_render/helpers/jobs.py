d84320a3825c884c826297eeb57930f15f402af7
mauriciolongato <mauriciolongato@gmail.com>
2019-05-18 16:01:23 -0300

    Imagem do codigo de barras recortada e html com a linha com tamanho setado

--- pdf_render/helpers/jobs.py
+++ pdf_render/helpers/jobs.py
@@ -1,12 +1,20 @@
+from PIL import Image
 import base64
 import barcode
 from barcode.writer import ImageWriter
+
+
+def crop_img(endereco):
+    img = Image.open(endereco)
+    w, h = img.size
+    img.crop((0, 50, w, h-130)).save(endereco)
 
 
 def create_barcode(codigo, barcode_class='code128'):
     img = barcode.get_barcode_class(barcode_class)
     img_bar = img('{}'.format(codigo), writer=ImageWriter())
     img_bar.save('static/{}'.format(codigo))
+    crop_img('static/{}.png'.format(codigo))
     return '{}.png'.format(codigo)
 
 
